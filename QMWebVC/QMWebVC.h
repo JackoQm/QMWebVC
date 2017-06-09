9057ff460388421cf9fe9764dd1dee46cbacc724
Jacko Qm <userqm@163.com>
2017-06-09 18:29:14 +0800

    Initial Commit

---
+++ QMWebVC/QMWebVC.h
@@ -0,0 +1,19 @@
+//
+//  QMWebVC.h
+//  QMWebVC
+//
+//  Created by Jacko Qm on 09/06/2017.
+//  Copyright © 2017 Jacko Qm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QMWebVC.
+FOUNDATION_EXPORT double QMWebVCVersionNumber;
+
+//! Project version string for QMWebVC.
+FOUNDATION_EXPORT const unsigned char QMWebVCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QMWebVC/PublicHeader.h>
+
+
